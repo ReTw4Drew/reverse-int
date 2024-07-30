70907ec08402a1842ac718968e4a9781c7bbbd58
Andrew <crowbarr90@mail.ru>
2024-07-30 10:49:34 +0700

    init: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let str = n.toString();
+    let strReverse = '';
+    for (i = str.length- 1; i >= 0; i--)
+        if (!(str[i] == '0') | !(str[i] == '-'))
+        strReverse = strReverse + str[i];
+    return parseInt(strReverse);
 }
